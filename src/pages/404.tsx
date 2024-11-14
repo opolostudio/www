153185c4b174ad5fae2afa452245cb61af659f76
Gustavo Villalta <396968+gvillalta99@users.noreply.github.com>
2024-11-14 14:50:18 -0300

    Primeiro deploy

---
+++ src/pages/404.tsx
@@ -0,0 +1,48 @@
+import * as React from "react"
+import { Link, HeadFC, PageProps } from "gatsby"
+
+const pageStyles = {
+  color: "#232129",
+  padding: "96px",
+  fontFamily: "-apple-system, Roboto, sans-serif, serif",
+}
+const headingStyles = {
+  marginTop: 0,
+  marginBottom: 64,
+  maxWidth: 320,
+}
+
+const paragraphStyles = {
+  marginBottom: 48,
+}
+const codeStyles = {
+  color: "#8A6534",
+  padding: 4,
+  backgroundColor: "#FFF4DB",
+  fontSize: "1.25rem",
+  borderRadius: 4,
+}
+
+const NotFoundPage: React.FC<PageProps> = () => {
+  return (
+    <main style={pageStyles}>
+      <h1 style={headingStyles}>Acho que você se perdeu</h1>
+      <p style={paragraphStyles}>
+        <br />
+        {process.env.NODE_ENV === "development" ? (
+          <>
+            <br />
+            Try creating a page in <code style={codeStyles}>src/pages/</code>.
+            <br />
+          </>
+        ) : null}
+        <br />
+        <Link to="/">Ir para a página inicial</Link>.
+      </p>
+    </main>
+  )
+}
+
+export default NotFoundPage
+
+export const Head: HeadFC = () => <title>Not found</title>
